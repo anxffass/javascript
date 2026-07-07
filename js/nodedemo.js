dd1e33176e98c8c00995d6254cb24224eb0884cf
Mohammed anfas <anfasanfuu766@gmail.com>
2026-07-07 12:57:50 +0530

    first commit

---
+++ js/nodedemo.js
@@ -0,0 +1,41 @@
+//alert(hello);
+
+
+/*let a = 10
+    a = 30
+
+console.log(a)
+
+let a ;
+let a = 60
+
+console.log(a)
+
+const b = 40
+
+
+console.log(b)
+*/
+
+
+
+
+let name = `anfas`
+console.log(typeof(name))
+
+let age;
+
+let score = 90
+
+let data = null
+
+let person = {
+  name: "anfas",
+  age: 21
+};
+
+console.log(person,name)
+
+let array1 = [10,22,30,"euuer","jdf"]
+
+console.log(array1)
